543564d04e69b6337989aa921dfe6027b314c157
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-07-12 01:19:09 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
-#define NULL ((void *)0)
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
-int _print_str(char *string);
-int _print_int(long int var);
 int _printf(const char *format, ...);
-int to_Binary(unsigned int n);
-int to_Octal(unsigned int n);
-int _power_recursion(int x, int y);
-int to_Hexa(unsigned int num);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endi
+#endif
