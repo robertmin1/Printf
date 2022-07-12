b1446671961141b85374be00e680a73179027184
Nganga <eliud.gachunji@gmail.com>
2022-07-12 09:05:31 -0700

    Editing writefuncs file

--- write_funcs.c
+++ write_funcs.c
@@ -9,7 +9,9 @@
  * Description: _putchar uses a local buffer of 1024 to call write
  * as little as possible
  */
+
 int _putchar(char c)
+
 {
 	static char buf[1024];
 	static int i;
@@ -32,7 +34,9 @@ int _putchar(char c)
  * @str: pointer to the string to print
  * Return: number of chars written
  */
+
 int _puts(char *str)
+
 {
 	register int i;
 
